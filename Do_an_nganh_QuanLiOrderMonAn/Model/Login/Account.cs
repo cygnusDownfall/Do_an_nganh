139b4bd2f1d2d04a58174d026730620b733e62e7
CygnusDownfall <phapsuhacam112@gmail.com>
2022-10-02 21:22:15 +0700

    add some class

---
+++ Do_an_nganh_QuanLiOrderMonAn/Model/Login/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Do_an_nganh_QuanLiOrderMonAn.Model.Login
+{
+    public class Account
+    {
+
+    }
+    public class Admin : Account
+    {
+
+    }
+    public class customer : Account
+    {
+
+    }
+}
--- Do_an_nganh_QuanLiOrderMonAn/Model/MonAn.cs
+++ Do_an_nganh_QuanLiOrderMonAn/Model/MonAn.cs
@@ -4,13 +4,22 @@ using System.Collections.Generic;
 
 namespace Do_an_nganh_QuanLiOrderMonAn.Model
 {
-    class MonAn
+    public class MonAn
     {
+        int ID;
+        public int id { get => ID; }
+
         string _TenMonAn;
         public string TenMonAn { get { return _TenMonAn; } set { _TenMonAn = value; } }
+
         int _gia;
         public int Gia { get { return _gia; } set { _gia = value; } }
 
-
+        public MonAn(int Id,string tenmon,int gia)
+        {
+            ID = Id;
+            _TenMonAn = tenmon;
+            _gia = gia;
+        }
     }
 }
